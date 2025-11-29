5a85d2e1fcabf9ea37dd219aa587979eeb6b2f6a
Bence Fekete <fekete965@gmail.com>
2025-11-29 10:20:34 +0000

    Add register and run functions to commands struct

--- main.go
+++ main.go
@@ -20,6 +20,23 @@ type commands struct {
 	handlers map[string]func(s *state, cmd command) error
 }
 
+func (c *commands) run(s *state, cmd command) error {
+	if fn, ok := c.handlers[cmd.name]; ok {
+		return fn(s, cmd)
+	}
+
+	return fmt.Errorf("command not found: %v", cmd.name)
+}
+
+func (c *commands) register(name string, fn func(s *state, cmd command) error) error {
+	if _, ok := c.handlers[name]; ok {
+		return fmt.Errorf("command already registered: %v", name)
+	}
+
+	c.handlers[name] = fn
+	return nil
+}
+
 func main() {
 	configFile, err := config.Read()
 	if err != nil {
