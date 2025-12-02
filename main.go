75596eae3988118e8c006fc76c122d9d6846d8ef
Bence Fekete <fekete965@gmail.com>
2025-12-02 08:47:46 +0000

    Add RSS models

--- main.go
+++ main.go
@@ -127,6 +127,25 @@ func handleUsers(s *state, cmd command) error {
 	return nil
 }
 
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+type RSSChannel struct {
+	Title       string    `xml:"title"`
+	Link        string    `xml:"link"`
+	Description string    `xml:"description"`
+	Item        []RSSItem `xml:"item"`
+}
+
+type RSSFeed struct {
+	Channel RSSChannel `xml:"channel"`
+}
+
 func main() {
 	configFile, err := config.Read()
 	if err != nil {
