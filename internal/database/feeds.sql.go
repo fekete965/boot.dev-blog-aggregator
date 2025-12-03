36763ce821b5a9d70285526f3b96ee1c2a8240a7
Bence Fekete <fekete965@gmail.com>
2025-12-03 13:17:31 +0000

    Add GetFeeds query

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -40,3 +40,44 @@ func (q *Queries) CreateFeed(ctx context.Context, arg CreateFeedParams) (Feed, e
 	)
 	return i, err
 }
+
+const getFeeds = `-- name: GetFeeds :many
+SELECT feeds.id AS feed_id, users.name as user_name, feeds.name as feed_name, feeds.url as feed_url FROM feeds
+INNER JOIN users on feeds.user_id = users.id
+ORDER BY feeds.name ASC
+`
+
+type GetFeedsRow struct {
+	FeedID   uuid.UUID
+	UserName string
+	FeedName string
+	FeedUrl  string
+}
+
+func (q *Queries) GetFeeds(ctx context.Context) ([]GetFeedsRow, error) {
+	rows, err := q.db.QueryContext(ctx, getFeeds)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetFeedsRow
+	for rows.Next() {
+		var i GetFeedsRow
+		if err := rows.Scan(
+			&i.FeedID,
+			&i.UserName,
+			&i.FeedName,
+			&i.FeedUrl,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -2,3 +2,8 @@
 INSERT INTO feeds (id, user_id, url, name)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
+
+-- name: GetFeeds :many
+SELECT feeds.id AS feed_id, users.name as user_name, feeds.name as feed_name, feeds.url as feed_url FROM feeds
+INNER JOIN users on feeds.user_id = users.id
+ORDER BY feeds.name ASC;
