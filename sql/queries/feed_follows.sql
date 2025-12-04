1519835b81af83cff1b06d32fbdc86a91e01f4f8
Bence Fekete <fekete965@gmail.com>
2025-12-04 09:00:58 +0000

    Add DeleteFeedFollow query

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -25,3 +25,8 @@ FROM feed_follows
 INNER JOIN feeds ON feed_follows.feed_id = feeds.id
 WHERE feed_follows.user_id = $1 
 ORDER BY feed_follows.created_at ASC;
+
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows
+WHERE
+  user_id = $1 AND feed_id = $2;
